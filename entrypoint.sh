73debd7530e19f4cfe5e27674f6dcebe44b891f5
Yash <yc.projectbuilds@gmail.com>
2024-01-26 09:55:54 +1100

    create docker entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# source ros2
+source /opt/ros/humble/setup.bash
+
+# build ros2 
+
+
+# source install
+
+
+# launch navigation in docker terminal
+
+
+# launch navigation controller in another terminal
+
+
+# Start an interactive shell
+# /bin/bash
